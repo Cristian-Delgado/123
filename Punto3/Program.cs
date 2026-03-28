449e3e50511653d1ed61cd8b276f49afee3c068e
Cristian Delgado <c6553708@gmail.com>
2026-03-28 12:35:46 -0300

    Add files via upload

---
+++ Punto3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Punto 3:
+// Se realiza la carga de 10 valores enteros por teclado. Se desea conocer:
+// a) La cantidad de valores ingresados negativos.
+// b) La cantidad de valores ingresados positivos.
+// c) La cantidad de múltiplos de 15.
+// d) El valor acumulado de los números ingresados que son pares.
+
+using System;
+
+class Punto3
+{
+    static void Main()
+    {
+        int negativos = 0, positivos = 0, multiplos15 = 0, sumaPares = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Ingrese un número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+                negativos++;
+            else if (num > 0)
+                positivos++;
+
+            if (num % 15 == 0)
+                multiplos15++;
+
+            if (num % 2 == 0)
+                sumaPares += num;
+        }
+
+        Console.WriteLine("Negativos: " + negativos);
+        Console.WriteLine("Positivos: " + positivos);
+        Console.WriteLine("Múltiplos de 15: " + multiplos15);
+        Console.WriteLine("Suma de pares: " + sumaPares);
+    }
+}
